b9bbb151272b1875dfd86768feb64dae7e721e0f
Alex <cfoorg@gmail.com>
2024-03-10 18:30:26 +0200

    fix

--- inventory_example.py
+++ inventory_example.py
@@ -341,6 +341,7 @@ def inventory_input(hashMap,_files=None,_data=None):
     return hashMap 
 
 def sample1_on_input(hashMap,_files=None,_data=None):
-    if hashMap.get("listener")=="btn_res": hashMap.put("toast",str(int(hashMap.get("a"))+int(hashMap.get("b"))))
+    if hashMap.get("listener")=="btn_res": 
+        hashMap.put("toast",str(int(hashMap.get("a"))+int(hashMap.get("b"))))
     
     return hashMap
